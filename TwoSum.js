12bb8ab89511276cb1a5176578b9aa1723fb7e1c
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2025-04-11 18:00:05 +0200

    Problem Solving

--- TwoSum.js
+++ TwoSum.js
@@ -1,11 +1,19 @@
 function twoSum(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  let sum = 0;
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
+    sum = nums[start] + nums[end];
+    if (sum > target) {
+      end--;
+    } else if (sum < target) {
+      start++;
+    } else {
+      return [start, end];
     }
   }
-  return [];
+  return { start, end };
 }
-const array = [3, 3];
-const target = 6;
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const target = 9;
 console.log(twoSum(array, target));
