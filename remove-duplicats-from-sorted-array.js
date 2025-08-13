3c121e02537552b312a9e77d5a2806fdece2e097
Baianat <baianat@Baianats-iMac.local>
2025-08-13 12:53:59 +0300

    Add Remove Duplicates From Sorted Array

---
+++ remove-duplicats-from-sorted-array.js
@@ -0,0 +1,16 @@
+function removeDuplicates(nums) {
+  let count = 0,
+    prev = Infinity,
+    index = 0;
+  for (const i of nums) {
+    if (i !== prev) {
+      count++;
+      nums[index++] = i;
+    }
+    prev = i;
+  }
+  return count;
+}
+
+console.log(removeDuplicates([1, 1, 2]));
+// "Add Remove Duplicates From Sorted Array"
